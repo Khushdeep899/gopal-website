3caafbc38d52e267e042af587290682a7d80237a
Khushdeep899 <khushdeep899@gmail.com>
2024-01-15 12:59:20 -0600

    project set up and installed dependencies

---
+++ app/page.js
@@ -0,0 +1,22 @@
+// index.js
+
+
+// import About from '../components/About'
+// import Contact from '../components/Contact'
+// import Main from '../components/Main'
+// import Projects from '../components/Projects'
+// import Skills from '../components/Skills'
+
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+   
+        <title>Gopal Drywalls</title>
+
+      <p> Test shit</p>
+
+    </div>
+  )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+        <div>
+            
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
