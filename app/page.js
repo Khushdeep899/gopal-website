2169943e6c1489d3ca3b63946714354d689577f1
hrkrnsidhu <harkaran.sidhu138@gmail.com>
2024-01-16 12:15:46 -0600

    merged changes

--- app/page.js
+++ app/page.js
@@ -7,35 +7,44 @@
 // import Projects from '../components/Projects'
 // import Skills from '../components/Skills'
 
-// import Head from 'next/head'
-import Main from '@/components/Main'
 import Navbar from '../components/Navbar'
 
-import styles from './page.module.css'
-
+import About from "@/components/About";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
-
-
     <div>
-      {/* <Head>
-        <title>Gopal Drywalls</title>
-        </Head> */}
-
-
-    
-      <Navbar />
-     
-      
-      <Main />
    
-      
+        <title>Gopal Drywalls</title>
+
+
+      <Navbar />
+       <Main/>
+        <About/>
 
     </div>
-    
   )
 }
 
 
+
+// the correct way: 
+// import Head from 'next/head';
+// // other imports
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <Head>
+//         <title>Gopal Drywalls</title>
+//         <meta name="description" content="We do mud tape" />
+//         {/* other head elements */}
+//       </Head>
+
+//       {/* your content */}
+//       <p> Test content</p>
+//     </div>
+//   );
+// }
 
